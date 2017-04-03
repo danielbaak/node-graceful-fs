30f43b9135dc74daf267219deec383abbe5ba8f5
daniel baak <daniel.baak@gmail.com>
2017-04-03 14:06:42 +0200

    replace fs.ReadStream and fs.WriteStream
    replace fs.ReadStream and fs.WriteStream with fs.FileReadStream and fs.FileWriteStream respectively

--- graceful-fs.js
+++ graceful-fs.js
@@ -162,11 +162,11 @@ function patch (fs) {
     WriteStream = legStreams.WriteStream
   }
 
-  var fs$ReadStream = fs.ReadStream
+  var fs$ReadStream = fs.FileReadStream
   ReadStream.prototype = Object.create(fs$ReadStream.prototype)
   ReadStream.prototype.open = ReadStream$open
 
-  var fs$WriteStream = fs.WriteStream
+  var fs$WriteStream = fs.FileWriteStream
   WriteStream.prototype = Object.create(fs$WriteStream.prototype)
   WriteStream.prototype.open = WriteStream$open
 
